41f6ded300272f03860da91cec95b38c14885d12
jcran <jcran@0x0e.org>
2012-10-23 16:03:48 -0500

    update_201210231600

--- modules/post/windows/gather/credentials/outlook.rb
+++ modules/post/windows/gather/credentials/outlook.rb
@@ -11,6 +11,7 @@ require 'msf/core'
 require 'rex'
 require 'msf/core/post/windows/registry'
 require 'msf/core/post/windows/priv'
+require 'msf/core/auxiliary/report'
 
 class Metasploit3 < Msf::Post
 
@@ -32,7 +33,7 @@ class Metasploit3 < Msf::Post
 				'License'       => MSF_LICENSE,
 				'Author'        => [ 'Justin Cacak'],
 				'Version'       => '$Revision$',
-				'Platform'      => [ 'windows' ],
+				'Platform'      => [ 'win' ],
 				'SessionTypes'  => [ 'meterpreter' ]
 			))
 	end
