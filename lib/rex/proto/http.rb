920bceeb42688aeb0c2f42248af8af951879395a
jcran <jcran@0x0e.org>
2012-12-13 16:02:56 -0600

    update_201212131601

--- lib/rex/proto/http.rb
+++
@@ -1,6 +0,0 @@
-# -*- coding: binary -*-
-require 'rex/proto/http/packet'
-require 'rex/proto/http/request'
-require 'rex/proto/http/response'
-require 'rex/proto/http/client'
-require 'rex/proto/http/server'
